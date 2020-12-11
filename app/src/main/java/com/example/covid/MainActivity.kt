66fd1bf00d8708ab978ed0b60915a1152f51170a
Muhammad Luthfirrohman <luthfiroh200@gmail.com>
2020-12-11 13:18:51 +0700

    first commit

---
+++ app/src/main/java/com/example/covid/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.covid
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        btn_start.setOnClickListener{
+            val intent = Intent(this@MainActivity, MainActivity2::class.java)
+            startActivity(intent)
+        }
+    }
+}
